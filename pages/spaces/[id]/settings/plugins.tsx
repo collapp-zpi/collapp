00bf0b377f29b21d07aa3f2a56f6801ecf0b5765
Karol Sitarz <ka.sitarz@gmail.com>
2021-11-09 13:17:12 +0100

    add basic WYSIWYG editor

--- pages/spaces/[id]/settings/plugins.tsx
+++ pages/spaces/[id]/settings/plugins.tsx
@@ -4,8 +4,94 @@ import { useRouter } from 'next/router'
 import Button from 'shared/components/button/Button'
 import { GoChevronLeft } from 'react-icons/go'
 import { SpaceSettingsButtons } from 'includes/spaces/components/SpaceSettingsButtons'
+import ReactGridLayout from 'react-grid-layout'
+import styled from 'styled-components'
+import { useState } from 'react'
+
+const BASE = 15
+
+const Tile = styled.div`
+  background-color: #e0e0e0;
+  border-radius: ${BASE * 10 * 0.175}px;
+  min-width: ${BASE * 10}px;
+  min-height: ${BASE * 10}px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1em;
+  font-weight: 600;
+
+  > div {
+    width: 48px;
+    height: 48px;
+    border-radius: 12px;
+    background: #c5c5c5;
+    margin-bottom: 6px;
+  }
+`
+
+const Container = styled.main`
+  width: ${BASE * 5.4 * 10}px;
+  margin: auto;
+`
+
+const INITIAL_DATA = [
+  {
+    i: 'To-do list',
+    x: 0,
+    y: 0,
+    w: 2,
+    h: 3,
+  },
+  {
+    i: 'Calendar',
+    x: 2,
+    y: 0,
+    w: 1,
+    h: 1,
+    maxW: 1,
+    maxH: 1,
+  },
+  {
+    i: 'Weather',
+    x: 3,
+    y: 0,
+    w: 2,
+    h: 1,
+  },
+  {
+    i: 'Agenda',
+    x: 2,
+    y: 1,
+    w: 3,
+    h: 2,
+  },
+  {
+    i: 'Chat',
+    x: 0,
+    y: 3,
+    w: 4,
+    h: 3,
+  },
+  {
+    i: 'Quick link',
+    x: 4,
+    y: 3,
+    w: 1,
+    h: 1,
+  },
+  {
+    i: 'Photo a day',
+    x: 4,
+    y: 4,
+    w: 1,
+    h: 2,
+  },
+]
 
 const SpacePluginSettings = () => {
+  const [layout, setLayout] = useState(INITIAL_DATA)
   const router = useRouter()
   const pathId = String(router.query.id)
   const id = pathId
@@ -29,7 +115,18 @@ const SpacePluginSettings = () => {
         </div>
         <div className="flex-grow">
           <div className="bg-white px-8 py-8 rounded-3xl shadow-2xl">
-            Plugins
+            <Container>
+              <ReactGridLayout
+                width={BASE * 5.4 * 10}
+                cols={5}
+                margin={[BASE, BASE]}
+                rowHeight={BASE * 10}
+                layout={layout}
+                onLayoutChange={(data) => setLayout(data)}
+              >
+                {layout.map(generateItem)}
+              </ReactGridLayout>
+            </Container>
           </div>
         </div>
       </div>
@@ -38,3 +135,12 @@ const SpacePluginSettings = () => {
 }
 
 export default SpacePluginSettings
+
+const generateItem = (data) => {
+  return (
+    <Tile data-grid={data} key={data.i}>
+      <div />
+      {data.i}
+    </Tile>
+  )
+}
