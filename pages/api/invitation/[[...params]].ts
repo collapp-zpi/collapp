9ac5c42b09c80f63a42c52783c2e44eed5bf8f68
PepeKwapien <pppkwapien@gmail.com>
2021-11-14 14:13:33 +0100

    endpoint to get invitation by id

---
+++ pages/api/invitation/[[...params]].ts
@@ -0,0 +1,39 @@
+import {
+  createHandler,
+  Get,
+  Patch,
+  Param,
+  NotFoundException,
+  Query,
+  ParseNumberPipe,
+  BadRequestException,
+  UnauthorizedException,
+} from '@storyofams/next-api-decorators'
+import { prisma } from 'shared/utils/prismaClient'
+import { NextAuthGuard, RequestUser, User } from 'shared/utils/apiDecorators'
+
+@NextAuthGuard()
+class Invitations {
+  @Get('/:id')
+  async getInvitation(@Param('id') id: string, @User user: RequestUser) {
+    const invitation = await prisma.invite.findFirst({
+      where: { id },
+    })
+
+    if (!invitation) {
+      throw new NotFoundException('The invitation was not found.')
+    }
+
+    const spaceUser = await prisma.spaceUser.findFirst({
+      where: { spaceId: invitation.spaceId, userId: user.id },
+    })
+
+    if (!!spaceUser) {
+      throw new BadRequestException('User is already a member of this space')
+    }
+
+    return invitation
+  }
+}
+
+export default createHandler(Invitations)
