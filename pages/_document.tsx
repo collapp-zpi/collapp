a4a33d5d54b7d98cec32715e8c5f24fad99f5360
kuba-budzynski <main.kubabudzynski@gmail.com>
2021-12-06 23:25:58 +0100

    Force push

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document {
                       r=o.createElement('script');r.async=1;
                       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                       a.appendChild(r);
-                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+                  })(window, document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
             }}
           ></script>
         </Head>
