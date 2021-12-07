ab1e356e6c888ca4fc72aeddc57ad7d6e396e63c
kuba-budzynski <main.kubabudzynski@gmail.com>
2021-12-07 01:34:50 +0100

    hotjar to smartlook

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,14 +13,12 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(h,o,t,j,a,r){
-                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                      h._hjSettings={hjid:2736044,hjsv:6};
-                      a=o.getElementsByTagName('head')[0];
-                      r=o.createElement('script');r.async=1;
-                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                      a.appendChild(r);
-                  })(window, document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+                window.smartlook||(function(d) {
+                  var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+                  var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+                  c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+                  })(document);
+                  smartlook('init', '0d6bebf7a01eb89a2c2685d1d46723b6a276d31e');`,
             }}
           ></script>
         </Head>
