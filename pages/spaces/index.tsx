c102388561be2556ccfb32fa41a58824b12b63f7
PepeKwapien <pppkwapien@gmail.com>
2021-12-11 05:34:44 +0100

    Added pagination to spaces list

--- pages/spaces/index.tsx
+++ pages/spaces/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { withFilters } from 'shared/hooks/useFilters'
+import { useFilters, withFilters } from 'shared/hooks/useFilters'
 import { useQuery } from 'shared/hooks/useQuery'
 import { LogoSpinner } from 'shared/components/LogoSpinner'
 import { CgMathPlus } from 'react-icons/cg'
@@ -9,8 +9,10 @@ import { withAuth } from 'shared/hooks/useAuth'
 import { ErrorInfo } from 'shared/components/ErrorInfo'
 import { Tooltip } from 'shared/components/Tooltip'
 import { defaultSpaceIcon, defaultUserIcon } from 'shared/utils/defaultIcons'
+import { Pagination } from 'shared/components/Pagination'
 
 const Spaces = () => {
+  const [, setFilters] = useFilters()
   const { data, error } = useQuery('spaces', '/api/spaces')
 
   return (
@@ -30,45 +32,54 @@ const Spaces = () => {
         </div>
       )}
       {!!data && !error && (
-        <div className="grid grid-cols-4 gap-6">
-          <Tooltip value="Create a new space" innerClassName="">
-            <Link href="/spaces/create" passHref>
-              <div className="border-dashed border-gray-400 text-gray-400 border-2 rounded-3xl h-40 flex items-center justify-center text-xl cursor-pointer hover:text-blue-500 hover:border-blue-500 transition-colors">
-                <CgMathPlus />
-              </div>
-            </Link>
-          </Tooltip>
-          {data?.entities.map(({ id, name, icon, users }) => (
-            <Link key={id} href={`/spaces/${id}`} passHref>
-              <div className="border-gray-300 rounded-3xl h-40 cursor-pointer bg-gray-50 shadow-xl p-6 flex flex-col transform hover:-translate-y-2 hover:shadow-2xl transition-all">
-                <div className="flex justify-between items-center">
-                  <img
-                    src={icon || defaultSpaceIcon}
-                    alt="Space icon"
-                    className="w-10 h-10 rounded-lg bg-gray-200 shadow-lg"
-                  />
-                  <div className="flex -space-x-2.5">
-                    {users.map(({ user }) => (
-                      <Tooltip
-                        value={user.name}
-                        key={user.id}
-                        innerClassName="flex transform hover:-translate-y-1 hover:shadow-sm shadow-none transition-all"
-                      >
-                        <img
-                          src={user?.image || defaultUserIcon}
-                          alt="User image"
-                          className="w-9 h-9 rounded-full bg-gray-200 border-2 border-gray-50"
-                        />
-                      </Tooltip>
-                    ))}
+        <div>
+          <div className="grid grid-cols-4 gap-6">
+            <Tooltip value="Create a new space" innerClassName="">
+              <Link href="/spaces/create" passHref>
+                <div className="border-dashed border-gray-400 text-gray-400 border-2 rounded-3xl h-40 flex items-center justify-center text-xl cursor-pointer hover:text-blue-500 hover:border-blue-500 transition-colors">
+                  <CgMathPlus />
+                </div>
+              </Link>
+            </Tooltip>
+            {data?.entities.map(({ id, name, icon, users }) => (
+              <Link key={id} href={`/spaces/${id}`} passHref>
+                <div className="border-gray-300 rounded-3xl h-40 cursor-pointer bg-gray-50 shadow-xl p-6 flex flex-col transform hover:-translate-y-2 hover:shadow-2xl transition-all">
+                  <div className="flex justify-between items-center">
+                    <img
+                      src={icon || defaultSpaceIcon}
+                      alt="Space icon"
+                      className="w-10 h-10 rounded-lg bg-gray-200 shadow-lg"
+                    />
+                    <div className="flex -space-x-2.5">
+                      {users.map(({ user }) => (
+                        <Tooltip
+                          value={user.name}
+                          key={user.id}
+                          innerClassName="flex transform hover:-translate-y-1 hover:shadow-sm shadow-none transition-all"
+                        >
+                          <img
+                            src={user?.image || defaultUserIcon}
+                            alt="User image"
+                            className="w-9 h-9 rounded-full bg-gray-200 border-2 border-gray-50"
+                          />
+                        </Tooltip>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="mt-auto font-bold text-lg h-6 truncate">
+                    {name}
                   </div>
                 </div>
-                <div className="mt-auto font-bold text-lg h-6 truncate">
-                  {name}
-                </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
+          </div>
+          <div className="mt-12">
+            <Pagination
+              page={data?.pagination.page}
+              pages={data?.pagination.pages}
+              setPage={(page) => setFilters({ page: String(page) })}
+            />
+          </div>
         </div>
       )}
     </Layout>
